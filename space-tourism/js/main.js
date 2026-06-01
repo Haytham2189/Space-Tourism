3f426320e984b2aebd6d29c28098f13dd42937b4
haytham kalai <hitoburn@hotmail.fr>
2026-06-01 10:47:54 +0100

    Add dynamic destination page

--- space-tourism/js/main.js
+++ space-tourism/js/main.js
@@ -1,3 +1,4 @@
+
 const mobileNavToggle = document.querySelector(".mobile-nav-toggle");
 
 const mobileNavClose = document.querySelector(".mobile-nav-close");
@@ -11,3 +12,78 @@ mobileNavToggle.addEventListener("click", () => {
 mobileNavClose.addEventListener("click", () => {
   navbarMenu.classList.remove("open");
 });
+
+
+/*============================== Destination ==================*/
+
+const destinationTabs =
+  document.querySelectorAll(".destination__tab");
+
+const destinationImage =
+  document.querySelector("#destination-image");
+
+const destinationName =
+  document.querySelector("#destination-name");
+
+const destinationDescription =
+  document.querySelector("#destination-description");
+
+const destinationDistance =
+  document.querySelector("#destination-distance");
+
+const destinationTravel =
+  document.querySelector("#destination-travel");
+
+function updateDestination(destination) {
+
+  destinationName.textContent =
+    destination.name;
+
+  destinationDescription.textContent =
+    destination.description;
+
+  destinationDistance.textContent =
+    destination.distance;
+
+  destinationTravel.textContent =
+    destination.travel;
+
+  destinationImage.src =
+    destination.images.png;
+
+  destinationImage.alt =
+    destination.name;
+}
+
+fetch("./data.json")
+  .then(response => response.json())
+  .then(data => {
+
+    /* Initial state = Moon */
+    updateDestination(data.destinations[0]);
+
+    destinationTabs.forEach((tab) => {
+
+      tab.addEventListener("click", () => {
+
+        /* Active tab */
+        destinationTabs.forEach((button) => {
+          button.classList.remove("active");
+        });
+
+        tab.classList.add("active");
+
+        /* Find destination */
+        const selectedDestination =
+          data.destinations.find(
+            destination =>
+              destination.name === tab.dataset.destination
+          );
+
+        updateDestination(selectedDestination);
+
+      });
+
+    });
+
+  });
